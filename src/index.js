c846aa140eab386d97f103e9c1480b1e7c073c8b
Antony Córdoba <anfa2091@gmail.com>
2023-09-13 01:09:52 -0500

    work

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const outputDiv = document.getElementById("output");
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+
+function fetchDataAndDisplay(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      outputDiv.innerHTML = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+document.getElementById("btn1").addEventListener("click", () => {
+  fetchDataAndDisplay(
+    "https://swapi.dev/api/people/?search=" + searchInput.value
+  );
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+  fetchDataAndDisplay(
+    "https://swapi.dev/api/films/?search=" + searchInput.value
+  );
+});
+
+document.getElementById("btn3").addEventListener("click", () => {
+  fetchDataAndDisplay(
+    "https://swapi.dev/api/starships/?search=" + searchInput.value
+  );
+});
+
+document.getElementById("btn4").addEventListener("click", () => {
+  fetchDataAndDisplay(
+    "https://swapi.dev/api/vehicles?search=" + searchInput.value
+  );
+});
+
+document.getElementById("btn5").addEventListener("click", () => {
+  fetchDataAndDisplay(
+    "https://swapi.dev/api/species/?search=" + searchInput.value
+  );
+});
+
+document.getElementById("btn5").addEventListener("click", () => {
+  fetchDataAndDisplay(
+    "https://swapi.dev/api/planets/?search=" + searchInput.value
+  );
+});
+
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchInput.value;
+  if (searchTerm) {
+    fetchDataAndDisplay(
+      `https://swapi.dev/api/?search${encodeURIComponent(searchTerm)}`
+    );
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchButton");
+    const resultsDiv = document.getElementById("results");
+
+    
+    searchButton.addEventListener("click", function () {
+        const searchTerm = searchInput.value;
+
+        if (searchTerm) {
+            // Limpiar resultados anteriores
+            resultsDiv.innerHTML = "Cargando...";
+
+            // Realizar la solicitud a la API de SWAPI
+            fetch(`https://swapi.dev/api/${searchTerm}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Error en la solicitud: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    resultsDiv.innerHTML = ""; // Limpiar el contenido anterior
+
+                    // Mostrar los datos de la respuesta en un formato legible
+                    const dataString = JSON.stringify(data, null, 4);
+                    resultsDiv.textContent = dataString;
+                })
+                .catch(error => {
+                    resultsDiv.textContent = `Hubo un error al realizar la búsqueda: ${error.message}`;
+                    console.error(error);
+                });
+        } else {
+            resultsDiv.textContent = "Por favor, ingresa un término de búsqueda.";
+        }
+    });
+});
